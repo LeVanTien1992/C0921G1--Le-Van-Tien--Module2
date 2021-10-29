c9df8990bf5a0d1421471eadb37f442f4aa929c7
Your Name <vantienam@gmail.com>
2021-10-29 09:16:58 +0700

    up bai tap

--- src/ss1_introduction_to_java/bai_tap/chuyen_doi_tien_te/Main.java
+++ src/ss1_introduction_to_java/bai_tap/chuyen_doi_tien_te/Main.java
@@ -1,4 +1,15 @@
 package ss1_introduction_to_java.bai_tap.chuyen_doi_tien_te;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        double rate = 23.000;
+        double usd ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("NHập vào số tiền muốn đổi: ");
+        usd = scanner.nextDouble();
+        double vnd = usd * rate ;
+        System.out.println("Giá trị vnd " +vnd);
+    }
 }
